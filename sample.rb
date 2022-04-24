7d793c06da59d796c3aea70fe02431319fff936d
emmafils <imoroug@gmail.com>
2022-04-24 20:36:57 +0100

    End slackbot

--- sample.rb
+++ sample.rb
@@ -1,15 +1,13 @@
 require "slack-notify"
 require 'clockwork'
-sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
-sample.notify("c'est le chat slackbot!!")
-# If you require the gem below, you will be able to use methods that indicate the time, such as minutes, so it is recommended to describe it.
-require 'active_support/time'
 
-module Clockwork
-  sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
-  every(3.minutes, "c'est le chat slackbot!!") do
-sample.notify("c'est le chat slackbot!!")
+sample = SlackNotify::Client.new(webhook_url: 'https://hooks.slack.com/services/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')
+
+include Clockwork
+
+handler do |job|
+  puts "Running #{job}"
+  sample.test
 end
-  # Here, describe the Clockwork method "How often do you process?"
-  # Here, write the processing and settings you want to send at regular intervals in slack
-end
+
+every(3.minutes, "général")
